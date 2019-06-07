9061fe739545bc139b271cfca2f63dc42ff8a6cc
Arya Pramudya <aryapramudya.gunadarma@gmail.com>
2019-06-07 22:17:52 +0700

    Factory and Seed

--- app/Question.php
+++ app/Question.php
@@ -10,14 +10,14 @@ use App\Category;
 class Question extends Model
 {
     public function user(){
-    	return $this->belongsTo(User);
+    	return $this->belongsTo(User::class);
     }
 
     public function replies(){
-    	return $this->hasMany(Reply);
+    	return $this->hasMany(Reply::class);
     }
 
     public function category(){
-    	return $this->belongsTo(Category);
+    	return $this->belongsTo(Category::class);
     }
 }
--- app/Reply.php
+++ app/Reply.php
@@ -10,14 +10,14 @@ use App\Like;
 class Reply extends Model
 {
     public function question(){
-    	return $this->belongsTo(Question);
+    	return $this->belongsTo(Question::class);
     }
 
     public function user(){
-    	return $this->belongsTo(User);
+    	return $this->belongsTo(User::class);
     }
 
     public function like(){
-    	return $this->hasMany(Like);
+    	return $this->hasMany(Like::class);
     }
 }
