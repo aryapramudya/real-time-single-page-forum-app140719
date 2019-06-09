ed5cd7894ebd35e036d8cba0a0477cdacfa86fa7
Arya Pramudya <aryapramudya.gunadarma@gmail.com>
2019-06-10 00:45:04 +0700

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ Route::apiResource('/category','CategoryController');
 Route::apiResource('/question/{question}/reply','ReplyController');
 Route::post('/like/{reply}','LikeController@likeIt');
 Route::delete('/like/{reply}','LikeController@unLikeIt');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+});
